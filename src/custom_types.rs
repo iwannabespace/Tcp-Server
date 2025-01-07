fcd5bdd4d4f3ead6604b955c7cbd418d1bc01b09
Ertuğrul Faruk Pişkin <thertu58@gmail.com>
2025-01-07 16:33:39 +0300

    add new message typesg

--- src/custom_types.rs
+++ src/custom_types.rs
@@ -1,5 +1,33 @@
+use serde::{Deserialize, Serialize};
+
 #[derive(Debug)]
 pub enum ChannelDataType {
     RemoveClient(u64),
     Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum MessageType {
+    Kicked,
+    Timeout(u64),
+    ConnectionDropped,
+    ClientMessage(u64),
+    ServerMessage(u64),
+    ClientDisconnected(u64),
+    NewClient(u64),
+    IdAssign(u64),
+    Whisper(u64, u64),
+    MessageDeserializeError,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    pub message_type: MessageType,
+    pub body: Option<Vec<u8>>,
+}
+
+impl Message {
+    pub fn new(message_type: MessageType, body: Option<Vec<u8>>) -> Self {
+        Self { message_type, body }
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,10 @@
 use std::io::{Read, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::{Arc, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
 use std::{io, thread};
-use std::net::{SocketAddr, TcpListener};
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{channel, Sender, Receiver};
 
 use crate::client::Client;
 use crate::custom_types::ChannelDataType;
@@ -20,8 +20,8 @@ pub struct TcpServerData {
     sender: Sender<ChannelDataType>,
     receiver: Mutex<Receiver<ChannelDataType>>,
     on_client_connected: OnClientConnectedCallback,
-    on_client_disconnected : OnClientDisconnectedCallback,
-    on_message_received: OnMessageReceivedCallback, 
+    on_client_disconnected: OnClientDisconnectedCallback,
+    on_message_received: OnMessageReceivedCallback,
 }
 
 impl TcpServerData {
@@ -29,17 +29,15 @@ impl TcpServerData {
         if let Ok(listener) = TcpListener::bind(address) {
             if listener.set_nonblocking(true).is_ok() {
                 let (sender, receiver) = channel::<ChannelDataType>();
-                Ok(
-                    Self {
-                        listener,
-                        sender,
-                        receiver: Mutex::new(receiver),
-                        clients: Arc::new(Mutex::new(Vec::new())),
-                        on_client_connected: Arc::new(Mutex::new(Box::new(|_, _| {}))),
-                        on_client_disconnected: Arc::new(Mutex::new(Box::new(|_| {}))),
-                        on_message_received: Arc::new(Mutex::new(Box::new(|_, _| {}))),
-                    }
-                )
+                Ok(Self {
+                    listener,
+                    sender,
+                    receiver: Mutex::new(receiver),
+                    clients: Arc::new(Mutex::new(Vec::new())),
+                    on_client_connected: Arc::new(Mutex::new(Box::new(|_, _| {}))),
+                    on_client_disconnected: Arc::new(Mutex::new(Box::new(|_| {}))),
+                    on_message_received: Arc::new(Mutex::new(Box::new(|_, _| {}))),
+                })
             } else {
                 Err("Listener couldn't be set to be non-blocking!".to_string())
             }
@@ -47,6 +45,12 @@ impl TcpServerData {
             Err("Listener couldn't be bind to address!".to_string())
         }
     }
+}
+
+pub enum SendOption {
+    All,
+    AllExcept(u64),
+    One(u64),
 }
 
 pub struct TcpServer {
@@ -57,7 +61,7 @@ pub struct TcpServer {
 impl TcpServer {
     pub fn new(address: &str) -> Result<Self, String> {
         let data = TcpServerData::new(address)?;
-        Ok(Self { 
+        Ok(Self {
             data: Arc::new(data),
             nonblocking: true,
         })
@@ -71,55 +75,78 @@ impl TcpServer {
         }
     }
 
-    pub fn send(&self, client_id: u64, data: &[u8]) -> Result<(), String> {
+    pub fn send(&self, send_option: SendOption, data: &[u8]) -> Result<(), String> {
         let data_ref = self.data.clone();
 
         if let Ok(clients) = data_ref.clients.lock() {
-            if let Some(client) = clients.iter().find(|client| client.id == client_id) {
-                let mut socket = &client.socket;
-                let header = (data.len() as u64).to_le_bytes();
-                let mut header_written: usize = 0;
-                let mut body_written: usize = 0;
-                
-                while header_written < 8 {
-                    match socket.write(&header[header_written..]) {
-                        Ok(size) => {
-                            if size > 0 {
-                                header_written += size;
-                            }
-                        },
-                        Err(e) => {
-                            if e.kind() == io::ErrorKind::WouldBlock {
-                                thread::sleep(Duration::from_millis(50));
-                            } else {
-                                return Err(e.kind().to_string());
-                            }
-                        }
-                    }
+            match send_option {
+                SendOption::All => {
+                    clients.iter().for_each(|client| {
+                        let socket = &client.socket;
+                        let _ = self.write_to_socket(socket, data);
+                    });
                 }
-
-                while body_written < data.len() {
-                    match socket.write(&data[body_written..]) {
-                        Ok(size) => {
-                            if size > 0 {
-                               body_written += size;
-                            }
-                        },
-                        Err(e) => {
-                            if e.kind() == io::ErrorKind::WouldBlock {
-                                thread::sleep(Duration::from_millis(50));
-                            } else {
-                                return Err(e.kind().to_string());
-                            }
+                SendOption::AllExcept(id) => {
+                    clients.iter().for_each(|client| {
+                        if client.id != id {
+                            let socket = &client.socket;
+                            let _ = self.write_to_socket(socket, data);
                         }
-                    }
+                    });
                 }
-
-                return Ok(());
+                SendOption::One(id) => {
+                    let client = clients.iter().find(|client| client.id == id).unwrap();
+                    let socket = &client.socket;
+                    let _ = self.write_to_socket(socket, data);
+                }
             }
+
+            return Ok(());
         }
 
         Err("Couldn't lock clients".to_string())
+    }
+
+    fn write_to_socket(&self, socket: &TcpStream, data: &[u8]) -> Result<(), String> {
+        let header = (data.len() as u64).to_le_bytes();
+        let mut header_written: usize = 0;
+        let mut body_written: usize = 0;
+
+        while header_written < 8 {
+            match { socket }.write(&header[header_written..]) {
+                Ok(size) => {
+                    if size > 0 {
+                        header_written += size;
+                    }
+                }
+                Err(e) => {
+                    if e.kind() == io::ErrorKind::WouldBlock {
+                        thread::sleep(Duration::from_millis(50));
+                    } else {
+                        return Err(e.kind().to_string());
+                    }
+                }
+            }
+        }
+
+        while body_written < data.len() {
+            match { socket }.write(&data[body_written..]) {
+                Ok(size) => {
+                    if size > 0 {
+                        body_written += size;
+                    }
+                }
+                Err(e) => {
+                    if e.kind() == io::ErrorKind::WouldBlock {
+                        thread::sleep(Duration::from_millis(50));
+                    } else {
+                        return Err(e.kind().to_string());
+                    }
+                }
+            }
+        }
+
+        Ok(())
     }
 
     fn run_nonblocking(&self) {
@@ -137,14 +164,18 @@ impl TcpServer {
 
                                 clients.push(Client::new(client_id, socket));
 
-                                if let Ok(on_client_connected) = data_ref.on_client_connected.lock() {
+                                if let Ok(on_client_connected) = data_ref.on_client_connected.lock()
+                                {
                                     on_client_connected(client_id, address);
                                 } else {
-                                    logger::log_to_file("logs.txt", "OnClientConnectedCallback couldn't be locked!");
+                                    logger::log_to_file(
+                                        "logs.txt",
+                                        "OnClientConnectedCallback couldn't be locked!",
+                                    );
                                 }
                             }
                         }
-                    },
+                    }
                     Err(e) => {
                         if e.kind() != io::ErrorKind::WouldBlock {
                             break;
@@ -157,11 +188,11 @@ impl TcpServer {
                         let mut socket_ref = &client.socket;
                         let mut amount_to_read: usize = 0;
                         let header_size = std::mem::size_of::<u64>();
-                        
+
                         if client.read_bytes >= 8 {
                             let arr: [u8; 8] = client.buffer[0..header_size].try_into().unwrap();
                             amount_to_read = usize::from_le_bytes(arr);
-                            
+
                             if client.buffer.len() != header_size + amount_to_read {
                                 client.buffer.resize(header_size + amount_to_read, 0);
                             }
@@ -170,28 +201,50 @@ impl TcpServer {
                         match socket_ref.read(&mut client.buffer[client.read_bytes..]) {
                             Ok(size) => {
                                 if size == 0 {
-                                    if let Err(e) = data_ref.sender.send(ChannelDataType::RemoveClient(client.id)) {
-                                        logger::log_to_file("logs.txt", format!("Channel data couldn't be sent!: {e}").as_str());
+                                    if let Err(e) = data_ref
+                                        .sender
+                                        .send(ChannelDataType::RemoveClient(client.id))
+                                    {
+                                        logger::log_to_file(
+                                            "logs.txt",
+                                            format!("Channel data couldn't be sent!: {e}").as_str(),
+                                        );
                                     }
-                                    if let Ok(on_client_disconnected) = data_ref.on_client_disconnected.lock() {
+                                    if let Ok(on_client_disconnected) =
+                                        data_ref.on_client_disconnected.lock()
+                                    {
                                         on_client_disconnected(client.id);
                                     } else {
-                                        logger::log_to_file("logs.txt", "OnClientDisconnectedCallback couldn't be locked");
+                                        logger::log_to_file(
+                                            "logs.txt",
+                                            "OnClientDisconnectedCallback couldn't be locked",
+                                        );
                                     }
                                 } else {
                                     client.read_bytes += size;
 
-                                    if amount_to_read > 0 && client.read_bytes == header_size + amount_to_read {
-                                        if let Ok(on_message_received) = data_ref.on_message_received.lock() {
-                                            on_message_received(client.id, client.buffer[header_size..].to_vec());
+                                    if amount_to_read > 0
+                                        && client.read_bytes == header_size + amount_to_read
+                                    {
+                                        // consider >= here
+                                        if let Ok(on_message_received) =
+                                            data_ref.on_message_received.lock()
+                                        {
+                                            on_message_received(
+                                                client.id,
+                                                client.buffer[header_size..].to_vec(),
+                                            ); // consider header_size..=amount_to_read here
                                             client.buffer.resize(8, 0);
                                             client.read_bytes = 0;
                                         } else {
-                                            logger::log_to_file("logs.txt", "OnMessageReceivedCallback couldn't be locked!");
+                                            logger::log_to_file(
+                                                "logs.txt",
+                                                "OnMessageReceivedCallback couldn't be locked!",
+                                            );
                                         }
                                     }
                                 }
-                            },
+                            }
                             Err(e) => {
                                 if e.kind() == io::ErrorKind::WouldBlock {
                                     thread::sleep(Duration::from_millis(50));
@@ -233,11 +286,14 @@ impl TcpServer {
                             if let Ok(on_client_connected) = data_ref.on_client_connected.lock() {
                                 on_client_connected(client_id, address);
                             } else {
-                                logger::log_to_file("logs.txt", "OnClientConnectedCallback couldn't be locked!");
+                                logger::log_to_file(
+                                    "logs.txt",
+                                    "OnClientConnectedCallback couldn't be locked!",
+                                );
                             }
                         }
                     }
-                },
+                }
                 Err(e) => {
                     if e.kind() != io::ErrorKind::WouldBlock {
                         break;
@@ -256,35 +312,60 @@ impl TcpServer {
 
                             match socket_ref.read_exact(&mut data) {
                                 Ok(_) => {
-                                    if let Ok(on_message_received) = data_ref.on_message_received.lock() {
+                                    if let Ok(on_message_received) =
+                                        data_ref.on_message_received.lock()
+                                    {
                                         on_message_received(client.id, data);
                                     } else {
-                                        logger::log_to_file("logs.txt", "OnMessageReceivedCallback couldn't be locked!");
+                                        logger::log_to_file(
+                                            "logs.txt",
+                                            "OnMessageReceivedCallback couldn't be locked!",
+                                        );
                                     }
-                                },
+                                }
                                 Err(e) => {
                                     if e.kind() == io::ErrorKind::UnexpectedEof {
-                                        if let Err(e) = data_ref.sender.send(ChannelDataType::RemoveClient(client.id)) {
-                                            logger::log_to_file("logs.txt", format!("Channel data couldn't be sent! {e}").as_str());
+                                        if let Err(e) = data_ref
+                                            .sender
+                                            .send(ChannelDataType::RemoveClient(client.id))
+                                        {
+                                            logger::log_to_file(
+                                                "logs.txt",
+                                                format!("Channel data couldn't be sent! {e}")
+                                                    .as_str(),
+                                            );
                                         }
-                                        if let Ok(on_client_disconnected) = data_ref.on_client_disconnected.lock() {
+                                        if let Ok(on_client_disconnected) =
+                                            data_ref.on_client_disconnected.lock()
+                                        {
                                             on_client_disconnected(client.id);
                                         } else {
-                                            logger::log_to_file("logs.txt", "OnClientDisconnectedCallback couldn't be locked");
+                                            logger::log_to_file(
+                                                "logs.txt",
+                                                "OnClientDisconnectedCallback couldn't be locked",
+                                            );
                                         }
                                     }
                                 }
                             }
-                        },
+                        }
                         Err(e) => {
                             if e.kind() == io::ErrorKind::UnexpectedEof {
-                                if let Err(e) = data_ref.sender.send(ChannelDataType::RemoveClient(client.id)) {
+                                if let Err(e) = data_ref
+                                    .sender
+                                    .send(ChannelDataType::RemoveClient(client.id))
+                                {
                                     println!("Channel data couldn't be sent!: {e}");
                                 }
-                                if let Ok(on_client_disconnected) = data_ref.on_client_disconnected.lock() {
+                                if let Ok(on_client_disconnected) =
+                                    data_ref.on_client_disconnected.lock()
+                                {
                                     on_client_disconnected(client.id);
                                 } else {
-                                     logger::log_to_file("logs.txt", "OnClientDisconnectedCallback couldn't be locked");
+                                    logger::log_to_file(
+                                        "logs.txt",
+                                        "OnClientDisconnectedCallback couldn't be locked",
+                                    );
                                 }
                             }
                         }
@@ -332,8 +413,8 @@ impl TcpServer {
     where
         F: Fn(u64, Vec<u8>) + Send + 'static,
     {
-        if let Ok(mut cb) = self.data.on_message_received.lock() { 
-           *cb = Box::new(callback);
+        if let Ok(mut cb) = self.data.on_message_received.lock() {
+            *cb = Box::new(callback);
         }
     }
 }
