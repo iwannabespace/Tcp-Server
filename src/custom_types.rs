8d08664061d47fe67520134b6f4a67289e506f35
Ertuğrul Faruk Pişkin <thertu58@gmail.com>
2025-01-13 14:33:10 +0300

    fixes

--- src/custom_types.rs
+++ src/custom_types.rs
@@ -5,29 +5,3 @@ pub enum ChannelDataType {
     RemoveClient(u64),
     Other,
 }
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum MessageType {
-    Kicked,
-    Timeout(u64),
-    ConnectionDropped,
-    ClientMessage(u64),
-    ServerMessage(u64),
-    ClientDisconnected(u64),
-    NewClient(u64),
-    IdAssign(u64),
-    Whisper(u64, u64),
-    MessageDeserializeError,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Message {
-    pub message_type: MessageType,
-    pub body: Option<Vec<u8>>,
-}
-
-impl Message {
-    pub fn new(message_type: MessageType, body: Option<Vec<u8>>) -> Self {
-        Self { message_type, body }
-    }
-}
